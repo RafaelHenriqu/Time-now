36c80b284ebaebfa1954864067b29ddb233fc412
Henrique <123227224+RafaelHenriqu@users.noreply.github.com>
2023-04-04 16:06:26 +0100

    Time-now v0.0.1
    modo escuro adicionado

--- Time.js
+++ Time.js
@@ -6,5 +6,13 @@ function TimeUpdate()
  const Horas = Data.getHours()
 document.getElementById('HorarioAgora').innerHTML = `${Horas}:${Minutos}:${Segundos} `
 }
+function Segredo()
+{
+
+document.querySelector('body').style.backgroundColor = '#191920'
+document.querySelector('body').style.color = 'rgb(221, 222, 223)'
+
+
+}
 
 setInterval(TimeUpdate,1000)
